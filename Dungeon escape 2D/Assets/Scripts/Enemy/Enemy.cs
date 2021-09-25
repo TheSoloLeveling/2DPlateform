b2c5ab479f185fe250b11838ed1879b3975155d6
bouziane yassine <62574566+TheSoloLeveling@users.noreply.github.com>
2021-09-25 12:05:55 +0200

    Enemy base class

--- Dungeon escape 2D/Assets/Scripts/Enemy/Enemy.cs
+++ Dungeon escape 2D/Assets/Scripts/Enemy/Enemy.cs
@@ -13,10 +13,52 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     protected Transform pointA, pointB;
 
-    public virtual void Attack()
-    {
+    protected Vector3 currentTarget;
+    protected Animator anim;
+    protected SpriteRenderer sr;
 
+    public virtual void Init()
+    {
+        currentTarget = Vector3.zero;
+        anim = GetComponentInChildren<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+    public void Start()
+    {
+        Init();
     }
 
-    public abstract void Update();
+    public virtual void Update()
+    {
+        movement();
+    }
+
+    public virtual void movement()
+    {
+        if (currentTarget == pointA.position)
+        {
+            sr.flipX = true;
+        }
+        else if (currentTarget == pointB.position)
+        {
+            sr.flipX = false;
+        }
+
+        if (transform.position == pointA.position)
+        {
+            anim.SetTrigger("Idle");
+            currentTarget = pointB.position;
+        }
+        else if (transform.position == pointB.position)
+        {
+            anim.SetTrigger("Idle");
+            currentTarget = pointA.position;
+        }
+
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+        }
+    }
+ 
 }
--- Dungeon escape 2D/Assets/Scripts/Enemy/MossGiant.cs
+++ Dungeon escape 2D/Assets/Scripts/Enemy/MossGiant.cs
@@ -4,48 +4,5 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    private Vector3 currentTarget;
-    private Animator anim;
-    private SpriteRenderer sr;
-
-    public void Start()
-    {
-        currentTarget = Vector3.zero;
-        anim = GetComponentInChildren<Animator>();
-        sr = GetComponentInChildren<SpriteRenderer>();
-    }
-    public override void Attack()
-    {
-
-    }
-
-    public override void Update()
-    {
-        if (currentTarget == pointA.position)
-        {
-            sr.flipX = true;
-        }
-        else if (currentTarget == pointB.position)
-        {
-            sr.flipX = false;
-        }
-
-        if (transform.position == pointA.position) 
-        {
-            anim.SetTrigger("Idle");
-            currentTarget = pointB.position;
-        }
-        else if (transform.position == pointB.position)
-        {
-            anim.SetTrigger("Idle");
-            currentTarget = pointA.position;
-        }
-
-        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
-        }
-        
     
-    }
 }
